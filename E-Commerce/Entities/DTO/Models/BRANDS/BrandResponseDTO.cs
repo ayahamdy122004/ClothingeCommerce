24b0a04fe212884cd8b45620cb1abfa9a7ac55d0
ayahamdy122004 <ayahamdy122004@gmail.com>
2026-08-25 23:07:50 +0300

    push and pull

---
+++ E-Commerce/Entities/DTO/Models/BRANDS/BrandResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿ِnamespace E_Commerce.Entities.DTO.Models.BRANDS
+{
+
+    public class BrandResponseDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string? LogoUrl { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
